737b0bae6aa4f056281d37f9cddc158a9426a7de
Mark Henry <markhenry.software@gmail.com>
2015-01-12 17:01:40 -0800

    Ready to begin exploration

--- serve.js
+++ serve.js
@@ -1,12 +1,10 @@
-//var ember = require('ember');
 var express = require('express');
-
-console.log("Loaded requires");
-
 var app = express();
 
+console.log('express.js loaded');
+
 app.set('port', (process.env.PORT || 5000));
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/app'));
 app.listen(app.get('port'), function () {
   console.log("App is listening on port " + app.get('port'));
 });
