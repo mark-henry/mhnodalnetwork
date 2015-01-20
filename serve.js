a82947a4a31687d461235f96b0ef17cef4c6ed03
Mark Henry <markhenry.software@gmail.com>
2015-01-20 14:33:09 -0800

    API returns graphs as adjacency lists

--- serve.js
+++ serve.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase(process.env['GRAPHSTORY_URL']);
+db.params = {userId: db.id};
+var Hashids = require('hashids');
+var hashids = new Hashids('Smaller cap, less plastic');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,19 +18,48 @@ app.use(function (req, res, next) {
 app.get('/api/graphs', function(req, res) {
   db.query('MATCH (g:Graph) RETURN g', {}, function(err, result) {
     if (err) throw err;
-    result.forEach(function(n) {
-      res.send(n['g'].toString());
+
+    var graphlist = [];
+
+    result.forEach(function(row) {
+      var dbid = row['g'].id;
+      var graphid = hashids.encode(dbid);
+      graphlist.push({id:graphid});
     });
+
+    res.json(graphlist);
   });
 });
 
 app.get('/api/graphs/:id', function(req, res) {
-  var query = 'MATCH (g:Graph {graph_id:' + req.params.id + '}) RETURN g';
-  db.query(query, {}, function(err, result) {
+  var query = ['MATCH (g:Graph)-[*]-(n1:Node)--(n2:Node)',
+    'WHERE id(g) = {graphid}',
+    'RETURN n1, n2'
+    ].join('\n');
+  var params = {graphid: hashids.decode(req.params.id)};
+  db.query(query, params, function(err, result) {
     if (err) throw err;
-    result.forEach(function(n) {
-      res.send(n['g'].toString());
+    if (result.length == 0) {
+      return res.status(404).send('Graph does not exist');
+    }
+
+    var graph = {nodes:{}, title:''};
+
+    result.forEach(function(row) {
+      var n1id = hashids.encode(row['n1'].id);
+      var n2id = hashids.encode(row['n2'].id);
+
+      if (!graph.nodes[n1id]) {
+        graph.nodes[n1id] = {};
+        graph.nodes[n1id].title = row['n1'].data.title;
+        graph.nodes[n1id].desc  = row['n1'].data.desc;
+        graph.nodes[n1id].adjacencies = [];
+      }
+      
+      graph.nodes[n1id].adjacencies.push(n2id);
     });
+
+    res.json(graph);
   });
 });
 
