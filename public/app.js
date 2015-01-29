e23b1c7537964acbc805e385f839eeee29103d9a
Mark Henry <markhenry.software@gmail.com>
2015-01-29 13:26:13 -0800

    Test nodes are drawn properly

--- public/app.js
+++ public/app.js
@@ -1,34 +1,147 @@
-
 App = Em.Application.create({
-  LOG_TRANSITIONS: true
+  LOG_TRANSITIONS: true,
+  ready: function() {
+    $(window).resize(function () {
+      App.graph.resize($(window).height(), $(window).width())
+    });
+
+    $("#charge").live('change', function() {
+      App.graph.set('charge', $(this).val())
+    })
+
+    App.graph.svg = d3.select("#chart").append("svg:svg")
+      .attr("width", App.graph.width).attr("height", App.graph.height);
+
+    this.graph.draw();
+  },
+  GraphView: Ember.View.extend({
+    templatename: 'graph'
+  })
+});
+
+App.Router.map(function() {
+  this.resource('graph', {path: '/graph/:graph_slug'}, function() {
+    this.resource('node', {path: '/node/:node_slug'});
+  });
 });
 
 App.Router.reopen({
   location: 'auto'
+})
+
+App.controls = Ember.Object.create({
+  lcfCodeError: null,
+  showPermalink: false,
+  isFullscreen: window.location == window.parent.location
+});
+
+App.graph = Ember.Object.create({
+  init: function() {
+    this.width =  $(window).width()
+    this.height = $(window).height()
+    this.rcx = this.width/2 + 110
+    this.rcy = this.height/2
+    this.radius = 240
+    this.colors = d3.scale.category10().range()
+    this.nodes = [] // the node with index 0 is fixed to the center and has a high charge
+    this.links = []
+    this.charge = 1000
+
+    this.force = d3.layout.force().charge(function(d, i) {
+      return i == 0 ? 0 : -App.graph.get('charge')
+    }).size([this.rcx*2, this.height]);
+
+    //TODO
+    this.force.on("tick", function(e) {
+      var graph = App.graph
+
+      graph.svg.selectAll("circle")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+
+      graph.svg.selectAll("line.link")
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+    });
+  },
+  resize: function(height, width){
+    this.svg.attr('height', height).attr('width', width)
+    this.set('height', height)
+    this.set('width', width)
+    this.set('rcx', width/2 + 110)
+    this.set('rcy', height/2)
+    this.force.size([this.rcx*2, height]);
+    this.redraw()
+  },
+  draw: function(){
+    var graph = App.graph
+
+    numVertices = 10
+
+    // magic vertex
+    this.nodes = [{fixed: false, x: this.rcx, y: this.rcy}]
+    // arrange nodes in a circle
+    this.nodes = this.nodes.concat(d3.range(numVertices).map(function(d, i) {
+      return {
+        x: this.rcx,
+        y: this.rcy
+      }
+    }))
+
+    graph.links = []
+    this.nodes.forEach(function(node, i) {
+      graph.links.push({source: graph.nodes[1], target: graph.nodes[i]})
+    })
+
+    this.force.nodes(this.nodes)
+    this.force.links(this.links)
+    this.force.start()
+    
+    this.drawLines()
+    this.drawCircles()
+  },
+  drawCircles: function() {
+    var circles = this.svg.selectAll("circle")
+    .data(this.nodes)
+    circles.enter()
+    .append("svg:circle")
+    circles.attr("r", function(d, i) { return i == 0 ? 0 : 5 })
+    .attr("class", function(d, i) { return i == 0 ? 'magic-vertex' : 'vertex' })
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .call(this.force.drag);
+    circles.exit().remove();
+  },
+  drawLines: function(){
+    var lines = this.svg.selectAll("line.link")
+    .data(this.links)
+    lines.enter()
+    .append("svg:line")
+    lines.attr("class", "link")
+    .attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; })
+    lines.exit().remove()
+  },
+  redraw: function() {
+      this.draw();
+  }.observes('charge')
 });
 
 DS.RESTAdapter.reopen({
   namespace: 'api'
 });
 
-App.Router.map(function() {
-  this.resource('graph', {path: '/graph/:id'}, function() {
-    this.resource('node', {path: '/node/:id'});
-  });
+App.Node = DS.Model.extend({
+  title: DS.attr('string'),
+  desc: DS.attr('string'),
+  adjacencies: DS.hasMany('node')
 });
 
 App.Graph = DS.Model.extend({
-  tile: DS.attr()
-});
-
-App.GraphRoute = Ember.Route.extend({
-  model: function(params) {
-    return this.store.find('graph', params.id);
-  }
-});
-
-App.NodeRoute = Ember.Route.extend({
-  model: function(params) {
-    return {id: params.node_id};
-  }
+  title: DS.attr('string'),
+  nodes: DS.hasMany('node')
 });
--- serve.js
+++ serve.js
@@ -5,6 +5,8 @@ var db = new neo4j.GraphDatabase(process.env['GRAPHSTORY_URL']);
 db.params = {userId: db.id};
 var Hashids = require('hashids');
 var hashids = new Hashids('Smaller cap, less plastic');
+var LRU = require('lru-cache');
+var cache = LRU({max: 500, maxAge: 1000 * 60 * 5});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,51 +18,103 @@ app.use(function (req, res, next) {
 });
 
 app.get('/api/graphs', function(req, res) {
-  db.query('MATCH (g:Graph) RETURN g', {}, function(err, result) {
-    if (err) throw err;
+  if (cache.has('graphlist')) {
+    res.json(cache.get('graphlist'));
+  }
+  else {
+    db.query('MATCH (g:Graph) RETURN g', {}, function(err, result) {
+      if (err) throw err;
 
-    var graphlist = [];
+      var graphlist = [];
 
-    result.forEach(function(row) {
-      var dbid = row['g'].id;
-      var graphid = hashids.encode(dbid);
-      graphlist.push({id:graphid});
+      result.forEach(function(row) {
+        var graphid = row['g'].id;
+        var graphslug = hashids.encode(graphid);
+        graphlist.push({slug:graphslug});
+      });
+
+      cache.set('graphlist', graphlist);
+      res.json(graphlist);
     });
-
-    res.json(graphlist);
-  });
+  }
 });
 
-app.get('/api/graphs/:id', function(req, res) {
-  var query = ['MATCH (g:Graph)-[*]-(n1:Node)--(n2:Node)',
-    'WHERE id(g) = {graphid}',
-    'RETURN n1, n2'
-    ].join('\n');
-  var params = {graphid: hashids.decode(req.params.id)};
-  db.query(query, params, function(err, result) {
-    if (err) throw err;
-    if (result.length == 0) {
-      return res.status(404).send('Graph does not exist');
-    }
-
-    var graph = {nodes:{}, title:''};
-
-    result.forEach(function(row) {
-      var n1id = hashids.encode(row['n1'].id);
-      var n2id = hashids.encode(row['n2'].id);
-
-      if (!graph.nodes[n1id]) {
-        graph.nodes[n1id] = {};
-        graph.nodes[n1id].title = row['n1'].data.title;
-        graph.nodes[n1id].desc  = row['n1'].data.desc;
-        graph.nodes[n1id].adjacencies = [];
+app.get('/api/nodes/:node_slug', function(req, res) {
+  var node_slug = req.params.node_slug;
+  if (cache.has(node_slug)) {
+    res.json(cache.get(node_slug));
+  }
+  else {
+    var node = db.getNodeById(hashids.decode(node_slug), function(err, result) {
+      if (err) throw err;
+      if (!result) {
+        res.status(404).send('Node does not exist');
       }
-      
-      graph.nodes[n1id].adjacencies.push(n2id);
-    });
+      else {
+        var node = {};
+        node.slug  = hashids.encode(result.id);
+        node.title = result.data.title;
+        node.desc  = result.data.desc;
+        node.adjacencies = [];
 
-    res.json(graph);
-  });
+        var query = 'MATCH (adj:Node)--(n:Node) WHERE id(n) = {nodeid} RETURN adj';
+        var params = {nodeid: result.id};
+        db.query(query, params, function(err, result) {
+          if (err) throw err;
+          result.forEach(function(row) {
+            adjacentslug = hashids.encode(row['adj'].id);
+            node.adjacencies.push(adjacentslug);
+          });
+          cache.set(node.slug, node);
+          res.json(node);
+        });
+      }
+    });
+  }
+});
+
+app.get('/api/graphs/:graph_slug', function(req, res) {
+  var graph_slug = req.params.graph_slug;
+  if (cache.has(graph_slug)) {
+    res.json(cache.get(graph_slug));
+  }
+  else {
+    var query = ['MATCH (g:Graph)-[*]-(n1:Node)--(n2:Node)',
+      'WHERE id(g) = {graphid}',
+      'RETURN n1, n2'
+      ].join('\n');
+    var params = {graphid: hashids.decode(req.params.graph_slug)};
+    db.query(query, params, function(err, result) {
+      if (err) throw err;
+      if (result.length == 0) {
+        res.status(404).send('Graph does not exist');
+      }
+      else {
+        var graph = {slug: req.params.graph_slug, nodes: []};
+        var nodesforcache = {};
+        result.forEach(function(row) {
+          var n1slug = hashids.encode(row['n1'].id);
+          var n2slug = hashids.encode(row['n2'].id);
+          graph.nodes.push(n1slug);
+
+          if (!nodesforcache[n1slug]) {
+            nodesforcache[n1slug] = {};
+            nodesforcache[n1slug].title = row['n1'].data.title;
+            nodesforcache[n1slug].desc  = row['n1'].data.desc;
+            nodesforcache[n1slug].adjacencies = [];
+          }
+          nodesforcache[n1slug].adjacencies.push(n2slug);
+        });
+
+        cache.set(graph.slug, graph);
+        Object.keys(nodesforcache).forEach(function(key) {
+          var node = nodesforcache[key];
+          cache.set(node.slug, node);
+        })
+        res.json(graph);
+      }
+    });
+  }
 });
 
 app.get('/*', function(req, res) {
